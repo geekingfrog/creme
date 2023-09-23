5a1cffd7192bf60e6f7e4cfad41ae1ae1cdc304d
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2023-09-23 08:25:22 +0100

    honeysql

--- src/creme/creme.clj
+++ src/creme/creme.clj
@@ -3,25 +3,37 @@
   (:import (com.zaxxer.hikari HikariDataSource))
   (:require
    [next.jdbc :as jdbc]
-   [next.jdbc.connection :as connection]))
+   [next.jdbc.connection :as connection]
+   [honey.sql :as sql]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args])
 
 (comment
+  (set! *warn-on-reflection* true)
   (let [dbname "creme"
         username "creme"
         dbspec
         {:jdbcUrl (str "jdbc:postgresql:" dbname ; "?user=" username
-                       "?socketFactory=org.newsclub.net.unix.AFUNIXSocketFactory$FactoryArg"
+                       "?user=" username
+                       "&socketFactory=org.newsclub.net.unix.AFUNIXSocketFactory$FactoryArg"
                        "&socketFactoryArg=/var/run/postgresql/.s.PGSQL.5432")
          :dataSourceProperties {:socketTimeout 30}}
         pool (connection/->pool com.zaxxer.hikari.HikariDataSource dbspec)]
 
-    (with-open [ds pool]
+    (with-open [^HikariDataSource ds pool]
       (.close (jdbc/get-connection ds))
-      (jdbc/execute! ds ["insert into blah (val) VALUES ('coucou1')"])
-      (jdbc/execute! ds ["select count(*) from blah"])
+      ; (jdbc/execute! ds (sql/format {:delete-from :blah}))
+      (let [c (-> (jdbc/execute! ds (sql/format {:select :%count.* :from [:blah]}))
+                  first
+                  :count)]
+        (jdbc/execute! ds (sql/format {:insert-into :blah
+                                       :columns [:val]
+                                       :values [[(format "coucou%03d" (inc c))]]}) #_["insert into blah (val) VALUES ('coucou1')"]))
+      (let [all-blah (jdbc/execute! ds (sql/format {:select [:*] :from [:blah]}))]
+        all-blah
+        )
+
       #_(jdbc/execute! ds ["select * from blah"])))
   :nil)
